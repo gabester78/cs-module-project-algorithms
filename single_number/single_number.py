2e93028f6fa187ca053206940bd06d9eac37cc89
Gabriel Romero <gabester78@gmail.com>
2020-08-19 19:29:44 -0500

    First two done.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,14 +2,24 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
+    # res = arr[0]
 
-    pass
+    # # Do XOR of all elements and return
+    # for i in range(1, len(arr) - 1):
+    #     res = res ^ arr[i]
+
+    # return res
+
+    return 2 * sum(set(arr)) - sum(arr)
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    # arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 1, 4, 4, 5, 5, 6, 3, 3, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
