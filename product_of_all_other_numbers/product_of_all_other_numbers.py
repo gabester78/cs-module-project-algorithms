9cad0e7286316fccc66bc4b2419e53ae5518d159
Gabriel Romero <gabester78@gmail.com>
2020-08-20 16:46:30 -0500

    MVP met.

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,34 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def product_of_all_other_numbers(arr):
-    # Your code here
 
-    pass
+
+def product_of_all_other_numbers(arr):
+
+    N = len(arr)
+
+    # base case
+    if N == 0:
+        return
+
+    # Initialzie list of 1, size N
+    l_prods, r_prods = [1]*N, [1]*N
+
+    for i in range(1, N):
+        l_prods[i] = l_prods[i-1] * arr[i-1]
+
+    for i in reversed(range(N-1)):
+        r_prods[i] = r_prods[i+1] * arr[i+1]
+
+    result = [x*y for x, y in zip(l_prods, r_prods)]
+    return result
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+           9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,46 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+
+def sliding_window_max(nums, k):
+
+    # grab length of nums array
+    n = len(nums)
+
+    # var to grab group's max number
+    max = 0
+
+    # hold max numbers appended from last loop from windows
+    arr = []
+
+    # loop through nums array arguement from nums[0] - nums[2]
+    for i in range(n - k + 1):
+
+        # updates max to current window being viewed
+        max = nums[i]
+
+        # loop through the window to find the highest value
+        for j in range(1, k):
+
+            # compares index of i from original loop to nested loop
+            # value of j
+            if nums[i + j] > max:
+
+                # if value is greater, add it to max var
+                max = nums[i + j]
+
+        # append value to arr var
+        arr.append(max)
+
+    return arr
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use queue instead of nested loops
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
